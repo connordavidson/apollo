93d4e2b80aaa610e8db8f9a9ce0d6d91d60b52d0
Connor Davidson <connor.j.davidson@gmail.com>
2020-07-14 08:16:01 -0400

    removed static from home directory and fixed error in GAE

---
+++ apollo_frontend/build/precache-manifest.ace316771854189d120d477957dc86e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000c0b77979342d5e768fba09b171c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0989faf1e096943b102d",
+    "url": "/static/css/2.c4383bce.chunk.css"
+  },
+  {
+    "revision": "ccd1bb957b508744002b",
+    "url": "/static/css/main.7bb28e14.chunk.css"
+  },
+  {
+    "revision": "0989faf1e096943b102d",
+    "url": "/static/js/2.60afd8f1.chunk.js"
+  },
+  {
+    "revision": "a63e334f3c3f3e10dbd5048e2dee05dc",
+    "url": "/static/js/2.60afd8f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd1bb957b508744002b",
+    "url": "/static/js/main.1ae797c6.chunk.js"
+  },
+  {
+    "revision": "d93e9a7d3d131d35b0ca",
+    "url": "/static/js/runtime-main.17a5e51f.js"
+  }
+]);
